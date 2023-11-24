e98628580003156dbd2b126efee4327b6235c821
mirco <116552555+mircoterenzi@users.noreply.github.com>
2023-11-24 10:37:31 +0100

    [ASN2] added active field and methods in task

--- assignment 2/src/arduino/smart_bridge/src/scheduler/Scheduler.cpp
+++ assignment 2/src/arduino/smart_bridge/src/scheduler/Scheduler.cpp
@@ -32,17 +32,8 @@ void Scheduler::schedule() {
     timerFlag = false;
 
     for (int i = 0; i < nTasks; i++) {
-        //if (taskList[i]->isActive()) {
-            //if (taskList[i]->isPeriodic()) {
-                if (taskList[i]->updateAndCheckTime(basePeriod)) {
-                    taskList[i]->tick();
-                }
-            /*} else {
-                taskList[i]->tick();
-                if(taskList[i]->isCompleted()) {
-                    taskList[i]->setActive(false);
-                }
-            }*/
-        //}
+        if (taskList[i]->isActive() && taskList[i]->updateAndCheckTime(basePeriod)) {
+            taskList[i]->tick();
+        }
     }
 }
