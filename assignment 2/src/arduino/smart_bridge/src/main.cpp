3636c651deea3d900be308e6375ee922a110a4f8
mirco <116552555+mircoterenzi@users.noreply.github.com>
2023-11-27 10:25:58 +0100

    [ASN2] impl of main

--- assignment 2/src/arduino/smart_bridge/src/main.cpp
+++ assignment 2/src/arduino/smart_bridge/src/main.cpp
@@ -1,18 +1,38 @@
 #include <Arduino.h>
+#include "config.h"
+#include "scheduler/Scheduler.h"
+#include "tasks/Task.h"
+#include "tasks/BlinkingTask.h"
+#include "tasks/DetectionTask.h"
+#include "tasks/GateControlTask.h"
+#include "tasks/WashControlTask.h"
 
-// put function declarations here:
-int myFunction(int, int);
+Scheduler scheduler;
+CarWasher* pCarWasher;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  scheduler.init(50);
+  pCarWasher = new CarWasher();
+  pCarWasher->init();
+
+  Task* pBlinkingTask = new BlinkingTask(LED02_PIN);
+  pBlinkingTask->init(2000, false);
+
+  Task* pDetectionTask = new DetectionTask(pCarWasher);
+  pDetectionTask->init(100);
+
+  Task* pGateControlTask = new GateControlTask(pCarWasher, pBlinkingTask);
+  pGateControlTask->init(100);
+
+  Task* pWashControlTask = new WashControlTask(pCarWasher, pBlinkingTask);
+  pWashControlTask->init(100);
+
+  scheduler.addTask(pBlinkingTask);
+  scheduler.addTask(pDetectionTask);
+  scheduler.addTask(pGateControlTask);
+  scheduler.addTask(pWashControlTask);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  scheduler.schedule();
 }
