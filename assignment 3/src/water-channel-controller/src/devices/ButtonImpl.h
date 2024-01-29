1c1a377799ddaaa06a47fb7235d9ba36a011f6ae
Giovanni Pisoni <giovanni.pisoni@studio.unibo.it>
2024-01-29 17:12:13 +0100

    [ASN3] Added ButtonImpl device

---
+++ assignment 3/src/water-channel-controller/src/devices/ButtonImpl.h
@@ -0,0 +1,21 @@
+#ifndef __BUTTONIMPL__
+#define __BUTTONIMPL__
+
+#include "Button.h"
+
+class ButtonImpl : public Button {
+    
+    public:
+        ButtonImpl(int pin);
+        bool isPressed() = 0;
+        bool isClicked() = 0;
+
+        void sync();
+    
+    private:
+        int pin;
+        bool pressed;
+        bool clicked;
+};
+
+#endif
