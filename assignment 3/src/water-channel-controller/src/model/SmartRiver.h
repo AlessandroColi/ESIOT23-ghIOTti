dcd972e22e1d43c0a12361fa703b0e1929b353cf
Giovanni Pisoni <giovanni.pisoni@studio.unibo.it>
2024-01-30 17:16:47 +0100

    [ANS3] Added first implementation of SmartRiver

---
+++ assignment 3/src/water-channel-controller/src/model/SmartRiver.h
@@ -0,0 +1,31 @@
+#ifndef __SMART_RIVER__
+#define __SMART_RIVER__
+
+#include <Arduino.h>
+#include <devices/Button.h>
+#include <devices/LCD.h>
+#include <devices/ServoMotor.h>
+#include <devices/Potentiometer.h>
+
+class SmartRiver
+{
+    public:
+        SmartRiver();
+        void init();
+        bool isButtonPressed();
+        void writeOnLCD(String text);
+        void servoOn();
+        void servoOff();
+        void setServoPosition(int percentageAngle);
+    
+    private:
+        double distance;
+        long time;
+        
+        Button* button;
+        Lcd* lcd;
+        ServoMotor* servoMotor;
+        Potentiometer* potentiometer;
+};
+
+#endif
