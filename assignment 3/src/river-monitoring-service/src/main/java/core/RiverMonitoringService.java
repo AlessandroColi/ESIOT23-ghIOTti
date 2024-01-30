1cf0b9277a5993ca631484f26667f7d8c4e78ca3
Coli Alessandro <Alessandro.coli2@studio.unibo.it>
2024-01-30 15:56:14 +0100

    [ASN3] backend main loop logic

--- assignment 3/src/river-monitoring-service/src/main/java/core/RiverMonitoringService.java
+++ assignment 3/src/river-monitoring-service/src/main/java/core/RiverMonitoringService.java
@@ -1,10 +1,71 @@
 package core;
 
-
 import mqtt.*;
 
+import java.nio.ByteBuffer;
+import java.util.concurrent.Flow;
+
 public class RiverMonitoringService {
-    public static void main(String[] args) {
+
+    private final static String backend = "backend";
+    private final static String esp = "esp32";
+    private final MqttProtocol espComm = new MqttProtocol();
+    private final stateControl state = new stateControl();
+
+    private boolean keepAlive = true;
+
+
+    public void run() {
+        espComm.initialize();
+        espComm.setupChannel(backend, esp);
+        espComm.readFromChannel(esp, backend).subscribe(new Flow.Subscriber<byte[]>() {
+                    @Override
+                    public void onSubscribe(Flow.Subscription subscription){
+                    }
+
+                    @Override
+                    public void onNext(byte[] level) {
+                        // Process the received message
+                        state.updateLevel(ByteBuffer.wrap(level).getDouble());
+                        updateAll(state.getUpdateFrequency(), state.getGateLevel());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        throw new RuntimeException("mqtt listen error: ", e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        keepAlive = false;
+                    }
+                });
+
+        while (keepAlive) {
+            try {
+                Thread.sleep(state.getUpdateFrequency());
+            } catch (InterruptedException e) {
+                throw new RuntimeException("loop error: ",e);
+            }
+        }
     }
 
+    private void updateAll(int frequency, int level) {
+        try {
+            espComm.writeToChannel(backend, esp, intToByteArray(frequency));
+        } catch (ProtocolError e) {
+            throw new RuntimeException("mqtt write error: ", e);
+        }
+    }
+
+    private byte[] intToByteArray(int value) {
+        ByteBuffer buffer = ByteBuffer.allocate(Integer.BYTES);
+        buffer.putInt(value);
+        return buffer.array();
+    }
+
+    public static void main(String[] args) {
+        RiverMonitoringService monitoringService = new RiverMonitoringService();
+        monitoringService.run();
+    }
 }
