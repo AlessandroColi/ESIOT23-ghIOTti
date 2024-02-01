cc97a5a277aa2a56e51e43ff0891ffb0c78ab754
Giovanni Pisoni <giovanni.pisoni@studio.unibo.it>
2024-02-01 16:15:26 +0100

    [ASN3] Made main.cpp and updated platformio.ini

--- assignment 3/src/water-channel-controller/src/main.cpp
+++ assignment 3/src/water-channel-controller/src/main.cpp
@@ -1,18 +1,44 @@
 #include <Arduino.h>
+#include "model/WaterController.h"
+#include "scheduler/Scheduler.h"
+#include "tasks/Task.h"
+#include "tasks/StateTask.h"
+#include "tasks/ValveTask.h"
+#include "tasks/CominicationTask.h"
+#include "config.h"
 
-// put function declarations here:
-int myFunction(int, int);
+Scheduler scheduler;
+WaterController* waterController;
+Lcd* lcd;
+Button* button;
+Potentiometer* potentiometer;
+ServoMotor* servoMotor;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  scheduler.init(200);
+
+  Serial.begin(9600);
+
+  servoMotor = new ServoMotorImpl(SERVO_PIN);
+  potentiometer = new Potentiometer(POT_PIN);
+  lcd = new Lcd();
+  button = new ButtonImpl(BUTTON_PIN);
+
+  waterController = new WaterController(button, servoMotor, lcd);
+
+  Task* stateTask = new StateTask(waterController);
+  Task* valveTask = new ValveTask(servoMotor, potentiometer, lcd, waterController); // Replace ValveTask with ValveTaskImpl
+  Task* comunicationTask = new ComunicationTask(waterController);
+
+  valveTask->init(200);
+  stateTask->init(200);
+  comunicationTask->init(200);
+
+  scheduler.addTask(valveTask);
+  scheduler.addTask(stateTask);
+  scheduler.addTask(comunicationTask);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  scheduler.schedule();
 }
