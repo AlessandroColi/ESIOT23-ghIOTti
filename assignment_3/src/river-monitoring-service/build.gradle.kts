1b9167842bb75afe806557583e83bb8721eb5cfd
mirco <116552555+mircoterenzi@users.noreply.github.com>
2024-02-09 18:03:48 +0100

    [ASN3] add vertx

--- assignment_3/src/river-monitoring-service/build.gradle.kts
+++ assignment_3/src/river-monitoring-service/build.gradle.kts
@@ -16,6 +16,13 @@ repositories {
 dependencies {
     implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
     implementation("io.github.java-native:jssc:2.9.5")
+    implementation("io.vertx:vertx-core:4.2.6")   
+    implementation("io.vertx:vertx-web:4.2.6")
+    implementation("io.vertx:vertx-web-client:4.2.6")
+    implementation("io.vertx:vertx-mqtt:4.2.6")
+    implementation("org.slf4j:slf4j-api:1.7.25")
+    implementation("org.slf4j:slf4j-jdk14:1.7.36")
+
 }
 
 tasks.test {
