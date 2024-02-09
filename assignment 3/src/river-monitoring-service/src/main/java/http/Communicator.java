3717329d2ae6a9f4b67a7aaca67fb7e27944e13a
mirco <116552555+mircoterenzi@users.noreply.github.com>
2024-02-09 15:11:15 +0100

    [ASN3] update interface

--- assignment 3/src/river-monitoring-service/src/main/java/http/Communicator.java
+++ assignment 3/src/river-monitoring-service/src/main/java/http/Communicator.java
@@ -1,15 +1,17 @@
 package http;
 
-import util.Pair;
+import java.util.Optional;
 
 public interface Communicator {
-    /*
-    sends the values to the dashboard
+    /**
+     * 
+     * @param waterLevel
+     * @param gateLevel
+     * @param state
      */
     public void write(double waterLevel, int gateLevel, String state);
-    /*
-    left side rapresents if the gate level is set manually by the dashjboard
-    right side rapresents the level set
+    /**
+     * @return empty if the gate level is set automatically, an integer if is set manually from the dashboard.
      */
-    public Pair<Boolean,Integer> check();
+    public Optional<Integer> check();
 }
